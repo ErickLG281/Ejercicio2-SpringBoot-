87056e4d60390c16e54476b1e6b46b358bfe4581
ErickLG <132631485+ErickLG281@users.noreply.github.com>
2024-11-09 15:18:07 -0500

    inicio

---
+++ src/main/java/com/erick/ViajeAerolinia/ViajeAeroliniaApplication.java
@@ -0,0 +1,13 @@
+package com.erick.ViajeAerolinia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajeAeroliniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajeAeroliniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erick/ViajeAerolinia/ViajeAeroliniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.ViajeAerolinia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajeAeroliniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
